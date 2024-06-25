1300653fc8a56610c68cb143f91f1f23eef8bb2c
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2024-06-25 14:13:21 +0200

    Get rid of anything else than Python

--- java-solution-holgers/src/main/java/foo/bar/service/ObservationSink.java
+++
@@ -1,8 +0,0 @@
-package foo.bar.service;
-
-
-import java.util.Collection;
-
-public interface ObservationSink {
-    void storeObservations(String observationName, Collection<String> exportOutput);
-}
--- ruby-solution/spec/spec_helper.rb
+++
@@ -1,6 +0,0 @@
-require_relative "../application.rb"
-
-RSpec.configure do |config|
-  config.color = true
-  config.full_backtrace=true
-end
