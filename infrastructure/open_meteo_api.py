254f8eaf584e7d995e5606e8aa102f16320ba54d
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2024-06-25 18:39:41 +0200

    Refactor OpenMeteoAPI class

--- infrastructure/open_meteo_api.py
+++ infrastructure/open_meteo_api.py
@@ -1,5 +1,7 @@
 import json
+
 import requests
+from dataclasses import dataclass
 
 from domain.weather_observation import WeatherObservationRepository, create_weather_observation, WeatherObservation
 
@@ -12,12 +14,13 @@ class OpenMeteoAPI(WeatherObservationRepository):
                     "-09&end_date=2024-06-23&hourly=temperature_2m,surface_pressure,wind_direction_100m")
 
     def get_all_weather_observations(self) -> list[WeatherObservation]:
-        response = requests.get(self.url)
-        response_dict = json.loads(response.content)
+        raw_observations = self.fetch_data()
 
-        raw_observations = response_dict['hourly']
+        return self.transform_raw_observations(raw_observations)
+
+    def transform_raw_observations(self, raw_observations) -> list[WeatherObservation]:
         number_of_observations = len(raw_observations)
-        observations = []
+        weather_observations_list = []
         for index in range(number_of_observations):
             observation = create_weather_observation(id=index,
                                                      name=self.station_name,
@@ -26,6 +29,11 @@ class OpenMeteoAPI(WeatherObservationRepository):
                                                      temperature=raw_observations['temperature_2m'][index],
                                                      pressure=raw_observations['surface_pressure'][index],
                                                      wind_direction=raw_observations['wind_direction_100m'][index])
-            observations.append(observation)
+            weather_observations_list.append(observation)
+        return weather_observations_list
 
-        return observations
+    def fetch_data(self) -> dict:
+        response = requests.get(self.url)
+        response_dict = json.loads(response.content)
+        raw_observations = response_dict['hourly']
+        return raw_observations
