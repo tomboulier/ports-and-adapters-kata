90bf1e1e28cda1a12466da98594b2b74250a8a8d
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2024-06-25 15:27:41 +0200

    Add attributes to WeatherObservation class

--- domain/weather_observation.py
+++ domain/weather_observation.py
@@ -1,2 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class WeatherObservation:
-    pass
+    id: int
+    name: str
+    date: str
+    time: str
+    temperature: float
+    pressure: float
+    wind_direction: str
--- tests/tests.py
+++ tests/tests.py
@@ -2,4 +2,10 @@ from domain.weather_observation import WeatherObservation
 
 
 def test_create_weather_observation():
-    observation = WeatherObservation()
+    observation = WeatherObservation(id=1,
+                                     name='Test',
+                                     date='2021-01-01',
+                                     time='12:00',
+                                     temperature=20.0,
+                                     pressure=1013.25,
+                                     wind_direction='N')
