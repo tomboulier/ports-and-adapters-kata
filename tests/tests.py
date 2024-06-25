847f5b21a438e48272b7d46e2f60664a027b4f9a
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2024-06-25 19:10:08 +0200

    Refactor tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,23 @@
 from domain.weather_observation import create_weather_observation, WeatherObservationRepository, WeatherObservation
 from infrastructure.open_meteo_api import OpenMeteoAPI
 
+LIST_OF_TWO_WEATHER_OBSERVATIONS = [
+    create_weather_observation(id=1,
+                               name='Test',
+                               date='2021-01-01',
+                               time='12:00',
+                               temperature=20.0,
+                               pressure=1013.25,
+                               wind_direction='N'),
+    create_weather_observation(id=2,
+                               name='Test 2',
+                               date='2021-01-01',
+                               time='12:00',
+                               temperature=20.0,
+                               pressure=1013.25,
+                               wind_direction='N')
+]
+
 
 def test_create_weather_observation():
     observation = create_weather_observation(id=1,
@@ -15,22 +32,7 @@ def test_create_weather_observation():
 
 class InMemoryWeatherObservationRepository(WeatherObservationRepository):
     def get_all_weather_observations(self) -> list[WeatherObservation]:
-        return [
-            create_weather_observation(id=1,
-                                       name='Test',
-                                       date='2021-01-01',
-                                       time='12:00',
-                                       temperature=20.0,
-                                       pressure=1013.25,
-                                       wind_direction='N'),
-            create_weather_observation(id=2,
-                                       name='Test 2',
-                                       date='2021-01-01',
-                                       time='12:00',
-                                       temperature=20.0,
-                                       pressure=1013.25,
-                                       wind_direction='N')
-        ]
+        return LIST_OF_TWO_WEATHER_OBSERVATIONS
 
 
 def test_get_weather_observation_from_in_memory_repository():
